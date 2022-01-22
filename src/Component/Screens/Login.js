abff138dfb28836f92fc1bb1f69c6658e3bfdf2d
benmansour <naoufelbenmensour@gmail.com>
2022-01-22 16:14:44 +0100

    add files

--- src/Component/Screens/Login.js
+++ src/Component/Screens/Login.js
@@ -5,8 +5,39 @@ import 'react-toastify/dist/ReactToastify.css';
 // import './main.css'
 
 const LoginScreen = () => {
+
+    const handleLogin = () => {
+        console.log("working")
+    }
+
     return (
-        <p>this is login Screen</p>
+        <div className="container-scroller">
+            <div className="container-fluid page-body-wrapper full-page-wrapper">
+                <div className="content-wrapper d-flex align-items-center auth px-0">
+                    <div className="row w-100 mx-0">
+                    <div className="col-lg-4 mx-auto">
+                        <div className="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <h4>Hello! let's get started</h4>
+                            <h6 className="font-weight-light">Sign in to continue.</h6>
+                            <form className="pt-3">
+                                <div className="form-group">
+                                <input type="email" className="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email" />
+                                </div>
+                                <div className="form-group">
+                                <input type="password" className="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div className="mt-3">
+                                <a className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" onClick={handleLogin} >SIGN IN</a>
+                                </div>
+                                <div className="my-2 d-flex justify-content-between align-items-center">
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
