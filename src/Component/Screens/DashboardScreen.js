3ad2416d9f051edf1a344b51def91dbeb012ffff
benmansour <naoufelbenmensour@gmail.com>
2022-01-22 18:59:42 +0100

    add files

--- src/Component/Screens/DashboardScreen.js
+++ src/Component/Screens/DashboardScreen.js
@@ -2,11 +2,16 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast, ToastContainer, Zoom } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import NavBarScreen from './subComponent/Navbar'
+import SideBarScreen from './subComponent/SideBar'
 // import './main.css'
 
 const DashboardScreen = () => {
     return (
-        <p>this is dashboard Screen</p>
+        <div class="container-scroller">
+            <NavBarScreen />
+            <SideBarScreen />
+        </div>
     )
 }
 
---
+++ src/Component/Screens/subComponent/SideBar.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toast, ToastContainer, Zoom } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+// import './main.css'
+
+const SideBarScreen = () => {
+    return (
+        <p>this is side bar screen</p>
+    )
+}
+
+export default SideBarScreen
