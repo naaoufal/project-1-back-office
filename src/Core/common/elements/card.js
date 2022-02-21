760b4645dfb03dade4008986418fb9c2cac72a81
benmansour <naoufelbenmensour@gmail.com>
2022-02-21 16:20:52 +0100

    add files

--- src/Core/common/elements/card.js
+++ src/Core/common/elements/card.js
@@ -1,40 +1,40 @@
 const Card = () => {
     return (
         <div className="row">
-            <div class="col-md-6 grid-margin stretch-card">
-                <div class="card tale-bg">
-                    <div class="card-people mt-auto">
+            <div className="col-md-6 grid-margin stretch-card">
+                <div className="card tale-bg">
+                    <div className="card-people mt-auto">
                         <img src="assets/images/dashboard/people.svg" alt="people" />
-                        <div class="weather-info">
-                            <div class="d-flex">
+                        <div className="weather-info">
+                            <div className="d-flex">
                                 <div>
-                                    <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
+                                    <h2 className="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
                                 </div>
-                            <div class="ml-2">
-                                <h4 class="location font-weight-normal">Essaouira</h4>
-                                <h6 class="font-weight-normal">Morocco</h6>
+                            <div className="ml-2">
+                                <h4 className="location font-weight-normal">Essaouira</h4>
+                                <h6 className="font-weight-normal">Morocco</h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             </div>
-            <div class="col-md-6 grid-margin transparent">
-                <div class="row">
-                    <div class="col-md-6 mb-4 stretch-card transparent">
-                        <div class="card card-tale">
-                            <div class="card-body">
-                                <p class="mb-4">Today’s Bookings</p>
-                                <p class="fs-30 mb-2">4006</p>
+            <div className="col-md-6 grid-margin transparent">
+                <div className="row">
+                    <div className="col-md-6 mb-4 stretch-card transparent">
+                        <div className="card card-tale">
+                            <div className="card-body">
+                                <p className="mb-4">Today’s Bookings</p>
+                                <p className="fs-30 mb-2">4006</p>
                                 <p>10.00% (30 days)</p>
                             </div>
                         </div>
                     </div>
-                <div class="col-md-6 mb-4 stretch-card transparent">
-                    <div class="card card-dark-blue">
-                        <div class="card-body">
-                            <p class="mb-4">Total Bookings</p>
-                            <p class="fs-30 mb-2">61344</p>
+                <div className="col-md-6 mb-4 stretch-card transparent">
+                    <div className="card card-dark-blue">
+                        <div className="card-body">
+                            <p className="mb-4">Total Bookings</p>
+                            <p className="fs-30 mb-2">61344</p>
                             <p>22.00% (30 days)</p>
                             </div>
                         </div>
