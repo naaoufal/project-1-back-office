d3ade7fd10da9484ce21a8c9e53e4016ac5d1510
benmansour <naoufelbenmensour@gmail.com>
2022-02-22 14:40:30 +0100

    add files

--- src/Core/common/elements/card.js
+++ src/Core/common/elements/card.js
@@ -40,6 +40,26 @@ const Card = () => {
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
+                  <div class="card card-light-blue">
+                    <div class="card-body">
+                      <p class="mb-4">Number of Meetings</p>
+                      <p class="fs-30 mb-2">34040</p>
+                      <p>2.00% (30 days)</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 stretch-card transparent">
+                  <div class="card card-light-danger">
+                    <div class="card-body">
+                      <p class="mb-4">Number of Clients</p>
+                      <p class="fs-30 mb-2">47033</p>
+                      <p>0.22% (30 days)</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
     )
