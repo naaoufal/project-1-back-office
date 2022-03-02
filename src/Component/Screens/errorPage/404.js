f234c9216919f23385543048185dcb2f98706c45
benmansour <naoufelbenmensour@gmail.com>
2022-03-02 15:21:26 +0100

    add classNames

--- src/Component/Screens/errorPage/404.js
+++ src/Component/Screens/errorPage/404.js
@@ -10,28 +10,28 @@ const ErrorScreen404 = () => {
     const navigation = useNavigate()
 
     return (
-        <div class="container-scroller">
-            <div class="container-fluid page-body-wrapper full-page-wrapper">
-                <div class="content-wrapper d-flex align-items-center text-center error-page bg-primary">
-                    <div class="row flex-grow">
-                        <div class="col-lg-7 mx-auto text-white">
-                            <div class="row align-items-center d-flex flex-row">
-                                <div class="col-lg-6 text-lg-right pr-lg-4">
-                                    <h1 class="display-1 mb-0">404</h1>
+        <div className="container-scroller">
+            <div className="container-fluid page-body-wrapper full-page-wrapper">
+                <div className="content-wrapper d-flex align-items-center text-center error-page bg-primary">
+                    <div className="row flex-grow">
+                        <div className="col-lg-7 mx-auto text-white">
+                            <div className="row align-items-center d-flex flex-row">
+                                <div className="col-lg-6 text-lg-right pr-lg-4">
+                                    <h1 className="display-1 mb-0">404</h1>
                                 </div>
-                                <div class="col-lg-6 error-page-divider text-lg-left pl-lg-4">
+                                <div className="col-lg-6 error-page-divider text-lg-left pl-lg-4">
                                     <h2>SORRY!</h2>
-                                    <h3 class="font-weight-light">The page you’re looking for was not found.</h3>
+                                    <h3 className="font-weight-light">The page you’re looking for was not found.</h3>
                                 </div>
                             </div>
-                            <div class="row mt-5">
-                                <div class="col-12 text-center mt-xl-2">
-                                    <a class="text-white font-weight-medium" href="#" onClick={() => navigation('/dashboard')}>Back to home</a>
+                            <div className="row mt-5">
+                                <div className="col-12 text-center mt-xl-2">
+                                    <a className="text-white font-weight-medium" href="#" onClick={() => navigation('/dashboard')}>Back to home</a>
                                 </div>
                             </div>
-                            <div class="row mt-5">
-                                <div class="col-12 mt-xl-2">
-                                    <p class="text-white font-weight-medium text-center">Copyright &copy; 2022  All rights reserved.</p>
+                            <div className="row mt-5">
+                                <div className="col-12 mt-xl-2">
+                                    <p className="text-white font-weight-medium text-center">Copyright &copy; 2022  All rights reserved.</p>
                                 </div>
                             </div>
                         </div>
